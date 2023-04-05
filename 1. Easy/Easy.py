87d3e0db8be5c6c2630741647736a3d2e31fac33
Valentina Marinova <61772169+Yuki69Okita@users.noreply.github.com>
2023-04-05 15:51:08 +0300

    Adding ex 6

--- 1. Easy/Easy.py
+++ 1. Easy/Easy.py
@@ -8,6 +8,7 @@ Then multiply them to display the output.
 """
 int_num = int(input("Integer number: "))
 float_num = float(input("Float number: "))
+
 print("Result: ", int_num * float_num)
 
 
@@ -18,11 +19,13 @@ Calculate the result of second number power of the first number.
 """
 base_num = int(input("Base number: "))
 power_num = int(input("Power number: "))
+
 print("Result: ", base_num ** power_num)
 
 # Second way
 base_num = int(input("Base number: "))
 power_num = int(input("Power number: "))
+
 print("Result: ", pow(base_num, power_num))
 
 
@@ -31,6 +34,8 @@ Ex. 3 - Create a random number
 Create a random number between 0 to 10.
 """
 import random
+
+
 print("Result: ", random.randint(0, 10))
 
 
@@ -40,12 +45,16 @@ Find the floor division of two numbers.
 """
 first_num = int(input("First number: "))
 second_num = int(input("Second number: "))
+
 print("Result: ", first_num // second_num)
 
 # Second way
 import math
+
+
 first_num = int(input("First number: "))
 second_num = int(input("Second number: "))
+
 print("Result: ", math.floor(first_num / second_num))
 
 
@@ -56,15 +65,31 @@ On the other hand, the value of the second variable will become the value of the
 """
 first_num = 2
 second_num = 3
+
 print(f"First value: {first_num}, Second value: {second_num}")
+
 first_num, second_num = second_num, first_num
+
 print(f"First value: {first_num}, Second value: {second_num}")
 
 # Second way
 first_num = 2
 second_num = 3
+
 print(f"First value: {first_num}, Second value: {second_num}")
+
 temp = first_num
 first_num = second_num
 second_num = temp
+
 print(f"First value: {first_num}, Second value: {second_num}")
+
+
+"""
+Ex. 6 - Sort a list
+Create a list and then sort it backwards.
+"""
+ex_list = [1, 4, 9, 1, 5]
+ex_list.sort(reverse=True)
+
+print(ex_list)
